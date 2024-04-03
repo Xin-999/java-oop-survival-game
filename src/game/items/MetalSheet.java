4c89d443839bedff8338e693b52a678ba5164991
Ang Qiao Xin <143700439+Xin-456@users.noreply.github.com>
2024-04-03 19:04:40 +0800

    Implement 'MetalSheet' item class
    - for resource gathering
    - Define character representation as '%' for the in-game visual display.
    - Mark MetalSheet as portable, allowing players to pick up and carry the item within their inventory.

---
+++ src/game/items/MetalSheet.java
@@ -0,0 +1,10 @@
+package game.items;
+import edu.monash.fit2099.engine.items.Item;
+public class MetalSheet extends Item{
+    /**
+     * Class representing a metal sheet item.
+     */
+    public MetalSheet() {
+        super("Metal Sheet", '%', true);
+    }
+}
