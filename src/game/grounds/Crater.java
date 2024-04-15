1f3d38b8efd8f2dbc076da732d77b700584cf412
Ang Qiao Xin <143700439+Xin-456@users.noreply.github.com>
2024-04-15 20:43:12 +0800

    Changed Crater class to abstract class
    - able to expand the code easily in the future to reduce repeated code.

--- src/game/grounds/Crater.java
+++ src/game/grounds/Crater.java
@@ -1,53 +1,29 @@
 package game.grounds;
 
 import edu.monash.fit2099.engine.actors.Actor;
-import edu.monash.fit2099.engine.positions.Exit;
 import edu.monash.fit2099.engine.positions.Ground;
 import edu.monash.fit2099.engine.positions.Location;
-import game.actors.HuntsmanSpider;
-import game.spawns.EnemyRegistry;
-import game.spawns.Spawner;
-
-import java.util.List;
 import java.util.Random;
-import java.util.function.Supplier;
 
-public class Crater extends Ground implements Spawner {
-    private static EnemyRegistry enemyRegistry = new EnemyRegistry();
+
+public abstract class Crater extends Ground {
+
     private Random random = new Random();
+    private int spawnRate;
 
-    static {
-        // Register known enemies here, HuntsmanSpider as an example
-        enemyRegistry.registerEnemy(HuntsmanSpider::new);
-        // Additional enemies can be registered similarly
-    }
 
-    public Crater() {
-        super('u'); // Representing crater with 'u'
+    public Crater(char displayChar, int spawnRate ) {
+        super(displayChar);
+        this.spawnRate = spawnRate;
     }
 
     @Override
     public void tick(Location location) {
-        if (random.nextInt(100) < 5) { // 5% chance to attempt to spawn an enemy
-            spawn(location);
+        if (random.nextInt(100) < spawnRate && !location.containsAnActor()) {
+            location.addActor(spawnCreature());
         }
     }
 
-    @Override
-    public void spawn(Location location) {
-        List<Exit> exits = location.getExits();
-        if (!exits.isEmpty()) {
-            // Choose a random exit for spawning
-            Exit exit = exits.get(random.nextInt(exits.size()));
-            Location spawnLocation = exit.getDestination();
+    protected abstract Actor spawnCreature();
 
-            // Check if the selected location is free
-
-                Actor enemy = enemyRegistry.getRandomEnemy();
-                if (enemy != null) {
-                    spawnLocation.addActor(enemy);
-                }
-
-        }
-    }
 }
